0cde014dfd5c2c49ba5693b340d0d10e120bc077
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-02-25 14:01:18 -0300

    Update the template and the code to generate the new template

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import mustache from "mustache";
 import axios from "axios";
+import * as cheerio from "cheerio";
 import { writeFileSync, readFileSync } from "fs";
 
 const API_URL = "https://api.github.com/users/freitagfelipe";
@@ -21,16 +22,39 @@ async function getHireableStatus() {
     return result.data.hireable;
 }
 
-async function getRepositoriesCount() {
+async function getTotalRepositories() {
     const result = await axios.get(`${API_URL}`);
 
     return result.data.public_repos;
 }
 
-async function getStarsCount() {
-    const result = await axios.get(`${API_URL}/starred`);
+async function getGithubStats() {
+    const URL =
+        "https://github-readme-stats.vercel.app/api?username=freitagfelipe&count_private=true";
+    const result = await axios.get(URL);
+    const $ = cheerio.load(result.data);
+    const githubStats = $("text[data-testid]")
+        .toArray()
+        .map((el) => $(el).text().trim());
 
-    return result.data.length;
+    return {
+        starsEarned: githubStats[1],
+        totalCommits: githubStats[2],
+        totalPullRequests: githubStats[3],
+        totalIssues: githubStats[4],
+    };
+}
+
+async function getMostUsedLanguages() {
+    const URL =
+        "https://github-readme-stats.vercel.app/api/top-langs/?username=freitagfelipe&hide_progress=true";
+    const result = await axios.get(URL);
+    const $ = cheerio.load(result.data);
+    const languages = $("text.lang-name")
+        .toArray()
+        .map((el) => $(el).text().trim());
+
+    return languages.join(", ");
 }
 
 const PATH = "./template.txt";
@@ -39,14 +63,20 @@ const content = readFileSync(PATH, "utf-8");
 
 const age = getAge();
 const hireableStatus = await getHireableStatus();
-const repositoriesCount = await getRepositoriesCount();
-const starredCount = await getStarsCount();
+const totalRepositories = await getTotalRepositories();
+const { starsEarned, totalCommits, totalPullRequests, totalIssues } =
+    await getGithubStats();
+const mostUsedLanguages = await getMostUsedLanguages();
 
 const README = mustache.render(content, {
     age,
     hireableStatus,
-    repositoriesCount,
-    starredCount
+    totalRepositories,
+    starsEarned,
+    totalCommits,
+    totalPullRequests,
+    totalIssues,
+    mostUsedLanguages,
 });
 
 writeFileSync("README.md", README);
